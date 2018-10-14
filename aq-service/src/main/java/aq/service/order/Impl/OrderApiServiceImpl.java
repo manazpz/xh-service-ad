44d524f3e18e5f421d86a442f4a23b1009cc94a4
manazpz <1348319927@qq.com>
2018-10-15 02:44:27 +0800

    v20181015

--- aq-service/src/main/java/aq/service/order/Impl/OrderApiServiceImpl.java
+++ aq-service/src/main/java/aq/service/order/Impl/OrderApiServiceImpl.java
@@ -44,52 +44,53 @@ public class OrderApiServiceImpl extends BaseServiceImpl  implements OrderApiSer
         JsonArray jsonArray = new JsonArray();
         Map<String,Object> res = new HashMap<>();
         Map<String,Object> ress = new HashMap<>();
+        List<Map<String, Object>> req = new ArrayList();
         res = GsonHelper.getInstance().fromJson(jsonObject,Map.class);
         List<Map<String, Object>> orders = orderDao.selectorderList(res);
         for (Map obj : orders) {
             String goodsName = "";
             ArrayList<Map> newOrder = new ArrayList();
             ArrayList<Map> oldOrder = new ArrayList();
-            res.put("id",obj.get("id"));
+            res.put("id", obj.get("id"));
             List<Map<String, Object>> orderPs = orderDao.selectorderDetailList(res);
             for (Map obj1 : orderPs) {
                 ress.clear();
-                ress.put("refId",obj1.get("goodsId"));
+                ress.put("refId", obj1.get("goodsId"));
                 List imgs = resourceDao.selectResource(ress);
-                obj1.put("imgs",imgs);
+                obj1.put("imgs", imgs);
                 String str = "";
                 Double banPrice = Double.parseDouble(obj1.get("banPrice").toString());
-                if(!StringUtil.isEmpty(obj1.get("parameter"))) {
+                if (!StringUtil.isEmpty(obj1.get("parameter"))) {
                     List<Map> parameter = GsonHelper.getInstance().fromJson(obj1.get("parameter").toString(), List.class);
                     for (Map obj2 : parameter) {
                         Double sum = 0.0;
                         List<Map> spec = (List<Map>) obj2.get("spec");
                         for (Map obj3 : spec) {
                             str += obj3.get("spec_value_name") + " ";
-                            if("02".equals(obj1.get("goodsModel"))){
+                            if ("02".equals(obj1.get("goodsModel"))) {
                                 Double correntPrice = Double.parseDouble(obj3.get("correntPrice").toString());
                                 Double minPrice = Double.parseDouble(obj3.get("minPrice").toString());
                                 Double cappedPrice = Double.parseDouble(obj3.get("cappedPrice").toString());
                                 Double price = banPrice + correntPrice;
-                                sum = sum + NumUtil.compareDouble(price,minPrice,cappedPrice)-banPrice;
+                                sum = sum + NumUtil.compareDouble(price, minPrice, cappedPrice) - banPrice;
                             }
                         }
-                        if("01".equals(obj1.get("goodsModel"))){
-                            obj1.put("guJia",obj2.get("price"));
+                        if ("01".equals(obj1.get("goodsModel"))) {
+                            obj1.put("guJia", obj2.get("price"));
                         }
-                        if("02".equals(obj1.get("goodsModel"))){
-                            obj1.put("guJia",sum +banPrice);
+                        if ("02".equals(obj1.get("goodsModel"))) {
+                            obj1.put("guJia", sum + banPrice);
                         }
                     }
                 }
-                obj1.put("parameterStr",str);
-                if("01".equals(obj1.get("goodsModel"))){
+                obj1.put("parameterStr", str);
+                if ("01".equals(obj1.get("goodsModel"))) {
                     newOrder.add(obj1);
                 }
-                if("02".equals(obj1.get("goodsModel"))){
+                if ("02".equals(obj1.get("goodsModel"))) {
                     oldOrder.add(obj1);
                 }
-                goodsName += obj1.get("goodsName")+ ";";
+                goodsName += obj1.get("goodsName") + ";";
             }
             Double newSum = 0.0;
             Double oldSum = 0.0;
@@ -101,20 +102,23 @@ public class OrderApiServiceImpl extends BaseServiceImpl  implements OrderApiSer
             for (Map m : oldOrder) {
                 oldSum += Double.parseDouble(m.get("guJia").toString());
             }
-            newMap.put("item",newOrder);
-            newMap.put("sum",newSum);
-            oldMap.put("item",oldOrder);
-            oldMap.put("sum",oldSum);
-            obj.put("newOrder",newMap);
-            obj.put("oldOrder",oldMap);
-            obj.put("sum",newSum-oldSum);
-            if(goodsName != ""){
-                obj.put("goodsName",goodsName);
+            newMap.put("item", newOrder);
+            newMap.put("sum", newSum);
+            oldMap.put("item", oldOrder);
+            oldMap.put("sum", oldSum);
+            obj.put("newOrder", newMap);
+            obj.put("oldOrder", oldMap);
+            obj.put("sum", newSum - oldSum);
+            if (goodsName != "") {
+                obj.put("goodsName", goodsName);
+            }
+            if (orderPs.size() > 0) {
+                req.add(obj);
             }
         }
         rtn.setCode(200);
         rtn.setMessage("success");
-        jsonArray =  GsonHelper.getInstanceJsonparser().parse(GsonHelper.getInstance().toJson(orders)).getAsJsonArray();
+        jsonArray =  GsonHelper.getInstanceJsonparser().parse(GsonHelper.getInstance().toJson(req)).getAsJsonArray();
         data.addProperty("total",jsonArray.size());
         data.add("items",jsonArray);
         rtn.setData(data);
