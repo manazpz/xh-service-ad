1028ad361407e4775848fadbd9320a74b8a5945c
manayxw <215828718@qq.com>
2018-12-03 17:49:39 +0800

    20181203

--- aq-common/src/main/java/aq/common/constants/APPConstants.java
+++ aq-common/src/main/java/aq/common/constants/APPConstants.java
@@ -9,18 +9,21 @@ public final class APPConstants {
 
     //服务器地址
     public static final String SPBILL_CREATE_IP = "47.106.177.213";
+//    public static final String SPBILL_CREATE_IP = "xa.huanyibu.com";
 
     //文件服务器地址
     public static final String FILE_SERVICE_URL = HTTP + SPBILL_CREATE_IP;
 //    public static final String FILE_SERVICE_URL = "D:\\Work\\resource\\contract\\";
 
     //附件资源位置
-    public static final String FILE_SERVICE_PLACE = "../../resouce/contract/xh";
+    public static final String FILE_SERVICE_PLACE = FILE_SERVICE_URL+":8081";
+//    public static final String FILE_SERVICE_PLACE = FILE_SERVICE_URL;
 //    public static final String FILE_SERVICE_PLACE = "D:\\Work\\resource\\contract";
 
 
     //支付回调
     public static final String WX_NOTIFY = FILE_SERVICE_URL + ":8083/aq/api/wx/notify";
+//    public static final String WX_NOTIFY = FILE_SERVICE_URL + "/aq/api/wx/notify";
 
     //数据库备份
     public static final String SQL_PATH = "../../sql/";
